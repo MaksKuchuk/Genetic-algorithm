105633d45fa2e99666e1c37411063b47832a264c
MaksKuchuk <94689394+MaksKuchuk@users.noreply.github.com>
2022-03-08 16:46:02 +1000

    Update settings

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ class Agent:
 	 	self.color = (rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255))
 	 	self.power = power
 	 	self.screen = screen
-	 	self.mutOfVectors = 15
-	 	self.selOfVectors = 10
+
+	 	#Settings Percents
+	 	self.mutOfVectors = 0.1
+	 	self.selOfVectors = 0.3
 
 	 def newAgent(self, ag):
 	 	self.moveV = copy.deepcopy(ag.moveV)
@@ -35,12 +37,12 @@ class Agent:
 	 	return (p[0]/l, p[1]/l)
 
 	 def mutate(self):
-	 	for i in range(int(len(self.moveV)/self.mutOfVectors)):
+	 	for i in range(int(len(self.moveV) * self.mutOfVectors)):
 	 		t = rd.randint(0, len(self.moveV)-1)
 	 		self.moveV[t] = self.mutRandNormV(self.moveV[t])
 
 	 def select(self, ag):
-	 	for i in range(int(len(self.moveV)/self.selOfVectors)):
+	 	for i in range(int(len(self.moveV) * self.selOfVectors)):
 	 		t = rd.randint(0, len(self.moveV)-1)
 	 		l = mt.sqrt((self.moveV[t][0] + ag.moveV[t][0])**2 + (self.moveV[t][1] + ag.moveV[t][1])**2)
 	 		if abs(l) > 0.0001:
@@ -63,22 +65,31 @@ class App:
 		self.res = res
 		self.screen = pg.display.set_mode(res, pg.SCALED)
 		self.clock = pg.time.Clock()
+		self.genPS = 0
 		self.fps = fps
+		self.startTime = time.time()
 		self.gen = 0
 		self.average = 0
 		self.best = 0
+		self.gameSpeed = 1/4000
 		self.localStep = 0
-		self.appSpeed = 0.0001
-		self.mutOfAgents = 10
-		self.selOfAgents = 10
-		self.partForDeletion = 25
+		self.appSpeed = 0.0001		
 		self.power = 5
 		self.steps = amSteps
+		self.dt = 0
+		self.maxRes = self.power * self.steps
 		self.prev_time = pg.time.get_ticks()
 		self.agents = [Agent(x, y, amSteps, self.power, self.screen) for x in range(int(res[0] / (amAgenX * 2)), res[0], int(res[0] / amAgenX)) for y in range(int(res[1]/(amAgenY * 2)), res[1], int(res[1]/amAgenY))]
 
+		#Settings Percents
+		self.mutOfAgents = 0.4
+		self.selOfAgents = 0.25
+		self.partForDeletion = 0.05
+
+
 	def run(self):
 		while True:
+			self.clock.tick
 			self.screen.fill('black')
 
 			self.control()
@@ -87,8 +98,9 @@ class App:
 			pg.display.flip()
 
 			[exit() for i in pg.event.get() if i.type == pg.QUIT]
+			self.dt = self.delta_time()
 			self.clock.tick(int(self.fps))
-			pg.display.set_caption(f'Generation: {self.gen}   Average: {self.average :.0f}   Best: {self.best :.0f}   FPS: {self.clock.get_fps() :.0f}')
+			pg.display.set_caption(f'{self.fps :.0f} Generation: {self.gen}   Average: {self.average :.0f}   Best: {self.best :.0f}   MaxRes: {self.maxRes :.0f}   GenInSec: {self.genPS :.0f}')
 
 	def draw(self):
 		for i in self.agents:
@@ -113,9 +125,9 @@ class App:
 		ags = [i[0] for i in t]
 
 		for i in range(len(ags)):
-			if (len(ags) / self.partForDeletion) <= i:
+			if (len(ags) * self.partForDeletion) <= i:
 				break
-			ags[i].newAgent(ags[rd.randint(int(len(ags) / self.partForDeletion), len(ags)-1)])
+			ags[i].newAgent(ags[rd.randint(int(len(ags) * self.partForDeletion), len(ags)-1)])
 
 		self.mutation(ags)
 		self.selection(ags)
@@ -123,6 +135,9 @@ class App:
 		for i in ags: i.xy = [0, 0]
 
 		self.agents = ags
+
+		self.genPS = 1/(time.time() - self.startTime)
+		self.startTime = time.time()
 
 	def getAverage(self):
 		s = 0
@@ -142,27 +157,26 @@ class App:
 		return time_now
 
 	def mutation(self, ags):
-		for i in range(int(len(ags)/self.mutOfAgents)):
+		for i in range(int(len(ags) * self.mutOfAgents)):
 			ags[rd.randint(0, len(ags)-1)].mutate()
 
 	def selection(self, ags):
-		for i in range(int(len(ags)/self.selOfAgents)):
+		for i in range(int(len(ags) * self.selOfAgents)):
 			ags[rd.randint(0, len(ags)-1)].select(ags[rd.randint(0, len(ags)-1)])
 
 	def control(self):
 		pressed_key = pg.key.get_pressed()
-		dt = self.delta_time()
 		if pressed_key[pg.K_w]:
 			self.step()
-		if pressed_key[pg.K_d]:
-			self.fps += dt * self.appSpeed
 		if pressed_key[pg.K_a]:
-			self.fps -= dt * self.appSpeed
+			self.fps -= self.dt * self.gameSpeed
+		if pressed_key[pg.K_d]:
+			self.fps += self.dt * self.gameSpeed
 
 
 if __name__ == '__main__':
-	xAgents = 6
-	yAgents = 6
+	xAgents = 10
+	yAgents = 10
 	steps = 30
 	fps = 30
 	app = App(xAgents, yAgents, steps, fps, (1280, 720))
